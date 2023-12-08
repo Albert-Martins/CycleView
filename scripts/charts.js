bc6619640979bccb272433abf0c0ff83a691aad6
Monalisa Gama <70865536+Monalisa-Gama@users.noreply.github.com>
2023-12-08 17:44:37 +0000

    de novo

--- scripts/charts.js
+++ scripts/charts.js
@@ -65,12 +65,20 @@ chart2 = new Chart(chart2Ctx, {
     data: {
       labels: ['Categoria 1', 'Categoria 2', 'Categoria 3', 'Categoria 4', 'Categoria 5'],
       datasets: [{
-        label: 'Valores',
+        label: '01',
         data: [10, 20, 15, 25, 18], // Valores para cada categoria
         backgroundColor: 'rgba(54, 162, 235, 0.2)', // Cor de preenchimento
         borderColor: 'rgba(54, 162, 235, 1)', // Cor da borda
         borderWidth: 1
-      }]
+    },
+    {
+        label: '02',
+        data: [8, 25, 16, 25, 2], // Valores para cada categoria
+        backgroundColor: 'rgba(54, 162, 255, 0.2)', // Cor de preenchimento
+        borderColor: 'rgba(54, 162, 235, 1)', // Cor da borda
+        borderWidth: 1
+    }
+    ]
     },
     options: {
       // Opções adicionais, como título, legenda, etc.
