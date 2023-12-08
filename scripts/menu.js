3a0ac898314bde6988257ce0fedd81412b227e85
Albert59-dev <albertsoares079@gmail.com>
2023-12-08 00:28:12 -0300

    Ajuetes visuais e comportamentais do menu de opções

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,22 @@
 function openMenu(){
     menu = document.querySelector('#menu');
     overlay = document.querySelector('#menuoverlay');
+    closeIcon = document.querySelector('#closeMenuIcon');
 
     overlay.style.display = 'inline-block';
     menu.style.left = 'auto';
     menu.style.right = '0%';
+    closeIcon.style.display = 'inline-block';
 
 }
 
 function closeMenu(){
     menu = document.querySelector('#menu');
     overlay = document.querySelector('#menuoverlay');
+    closeIcon = document.querySelector('#closeMenuIcon');
 
     overlay.style.display = 'none';
     menu.style.left = '100%';
     menu.style.right = 'auto';
+    closeIcon.style.display = 'none';
 }
